210c1d353622274d6e4eed65266ed40770a5e995
kachynskai <105295714+kachynskai@users.noreply.github.com>
2024-03-18 04:25:15 +0200

    creating empty functions in input.py and output.py

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_data_from_console():
+    pass
+
+
+def input_data_from_file(path_to_file):
+    pass
+
+
+def input_data_with_pandas(path_to_file):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def output_data_to_console(output_data):
+    pass
+
+
+def output_data_to_file(output_data, filename):
+    pass
+
+
+def output_data_with_pandas(output_data, filename):
+    pass
